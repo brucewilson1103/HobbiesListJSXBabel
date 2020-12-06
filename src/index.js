c971c7588ba74cb98852bf6b63778e5f323eb1c7
Bruce Wilson <brucewilson1103@gmail.com>
2020-12-05 21:14:25 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Favorite Hobbies</h1>
+    <ul>
+      <li>Coding</li>
+      <li>Riding Motorcycles</li>
+      <li>Playing Guitar</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
